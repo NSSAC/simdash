70efe2765cd4105ca936d23f32cd652eebc102c7
Kieran Heese <kh8fb@virginia.edu>
2019-07-18 14:35:09 -0400

    Fix database.py and test_db_and_tab.py
    Revised database.py to work with the first two columns being the
    logical time and real time columns of the table. Also changed some
    properties of table.py to be private rather than public as was discussed
    at the previous code review. Updated various docstrings in table.py and
    database.py to be more succint.  Finally moved test_db_and_tab.py to the
    test_simdash folder so that all tests could be run at once.

--- simdash/database/database.py
+++ simdash/database/database.py
@@ -14,19 +14,15 @@ from .table import Table
 
 class Database:
     """
-    Creates a Database based upon a given database file name
+    A Database is a collection of Tables with a variable meta_table that holds information for the other tables.
 
-    A Database initializes the meta_table and allows the creation of additional tables
-    within the given database file with the make_table function.  These additional tables
-    can then be retrieved and edited as a Table with the get_table function.
-
-    Attributes:
-        conn (sqlite3 connection): The connection to the database file
+    A Database can create Tables and keep track of each Table's columns, variable types for Altair charting,
+    as well as which variables are the logical time and real time.  This information is stored in the Database's
+    "meta_table" and allows for simple addition of data points to each table.
     """
-    
     def __init__(self, filename):
         """
-        Initializes the database connection and meta table.
+        Initialize the database connection and meta table.
 
         Args:
             filename (str): Path to the file where you want the database to be created, should end in .db
@@ -35,44 +31,37 @@ class Database:
             try:
                 self.conn = sqlite3.connect(filename)
                 with self.conn:
-                    c = self.conn.cursor()
+                    curs = self.conn.cursor()
                     create_table_string = """CREATE TABLE IF NOT EXISTS
                     meta_table(table_name TEXT, columns TEXT, dtypes TEXT,
                     vtypes TEXT, l_time_column TEXT, r_time_column TEXT);"""
-                    c.execute(create_table_string)
-            except sqlite3.Error as er:
-                print("SQLite error: %s" %er)
+                    curs.execute(create_table_string)
+            except sqlite3.Error as err:
+                print("SQLite error: %s" %err)
 
-    def make_table(self, table_name, columns, dtypes, vtypes, l_time_column="l_time", r_time_column="r_time"):
+    def make_table(self, table_name, columns, dtypes, vtypes):
         """
-        Inserts a row into meta table with this information and creates the according table in the SQL file.
+        Insert a row into meta table with this table's information and create the according Table.
 
-        Inserts a row into the meta table containing the columns, datatypes, variable types and the names of
-        the logical and real time variables of the table "table_name". Then this table is created elsewhere
-        in your SQL database.
-
+        Insert a row into the meta table containing the columns, datatypes, and variable types of
+        the table "table_name". Then this Table is also created.  The first item of columns must
+        be the variable name that is used for logical time, and the second item must be the variable
+        name that is used for real time.
         Args:
-            table_name (str): name of your desired table, this will be it's name in the SQL file and
-                proper care should be taken to ensure that the same table is not made twice
-            columns (list(str)): The names of the columns of your new table.  Should include the logical
-                time and real time columns even if they are specified in the function header.
-            dtypes (list(str)): The datatypes or SQL datatypes of each of the columns that have been passed in.
-                Supported now are INT, FLOAT, and TEXT
+            table_name: Name of your desired table. Proper care should be taken to ensure that the same table
+                 is not made twice
+            columns (list(str)): Names of the columns of your new table. The first two items in this list must
+                be the logical time column and the real time column respectively
+            dtypes (list(str)): The datatypes of each of the columns that have been passed in. Supported now are
+                INT, FLOAT, and TEXT.  Each index of dtypes should match with the column at that index in
+                columns.
             vtypes (list(str)): The variable types or Altair encodings of each of the passed in columns, must be 'Q',
-                'T', 'O', or 'N'
-            l_time_column (str): the name of the column that represents the logical time of the graphic,
-                defaults to 'l_time' and the value of this variable must also be in columns
-            r_time_column (str): the name of the column that represents the temporal time of the graphic,
-                defaults to 'r_time' and the value of this variable must also be in columns
+                'T', 'O', or 'N'.  Each index of vtypes should match with the column at that index in columns.
         Raises:
             ValueError: If the length of columns, vtypes or dtypes is different
             ValueError: If any dtype is not 'INT', 'FLOAT' or 'TEXT'
             ValueError: If any vtype is not 'N', 'O', 'T', or 'Q'
-            ValueError: If the logical time column is not in columns
-            ValueError: If the real time column is not in columns
             UserWarning: If the table has already been created in the file
-        Returns:
-            None
         """
         possible_dtype_list = ["INT", "FLOAT", "TEXT"]
         possible_vtype_list = ["Q", "T", "O", "N"]
@@ -84,58 +73,51 @@ class Database:
         for value in vtypes:
             if value not in possible_vtype_list:
                 raise ValueError("vtype %s is not of the correct type, must be 'N' 'O', 'T' or 'Q'" %value)
-        if l_time_column not in columns:
-            raise ValueError("Logical time column %s must be in the column list" %l_time_column)
-        if r_time_column not in columns:
-            raise ValueError("Real time column %s must be in the column list" %r_time_column)
+
+        # create the meta_table
         with self.conn:
-            c = self.conn.cursor()
-            already_created = False
+            curs = self.conn.cursor()
             sql_get_table_names = "SELECT name FROM sqlite_master WHERE type='table';"
-            for row in c.execute(sql_get_table_names):
+            for row in curs.execute(sql_get_table_names):
                 if row[0] == table_name:
-                    already_created = True
-                    if already_created:
-                        warnings.warn("This table has already been created", UserWarning)
-                        return
+                    warnings.warn("This table has already been created", UserWarning)
+                    return
             sql_insert_meta_string = "INSERT INTO meta_table VALUES(?, ?, ?, ?, ?, ?);"
             insert_meta_tuple = (table_name, json.dumps(columns), json.dumps(dtypes),
-                                 json.dumps(vtypes), l_time_column, r_time_column)
-            c.execute(sql_insert_meta_string, insert_meta_tuple)
-            sql_create_table_string = "CREATE TABLE IF NOT EXISTS {tn}({rt} INT, {lt} FLOAT);"
-            c.execute(sql_create_table_string.format(tn=table_name, rt=r_time_column, lt=l_time_column))
-            for i, value in enumerate(columns, 0): # append all other columns onto the table
-                if value not in(l_time_column, r_time_column):
-                    sql_alter_table = 'ALTER TABLE {tn} ADD COLUMN "{cn}" "{ct}";'
-                    c.execute(sql_alter_table.format(tn=table_name, cn=value, ct=dtypes[i]))
+                                 json.dumps(vtypes), columns[0], columns[1])
+            curs.execute(sql_insert_meta_string, insert_meta_tuple)
+
+            # Create the table
+            sql_create_table_string = f"CREATE TABLE IF NOT EXISTS {table_name}({columns[0]} FLOAT);"
+            curs.execute(sql_create_table_string)
+            for i, value in enumerate(columns[1:], 1): 
+                sql_alter_table = 'ALTER TABLE {tn} ADD COLUMN "{cn}" "{ct}";'
+                curs.execute(sql_alter_table.format(tn=table_name, cn=value, ct=dtypes[i]))
             return
 
     def get_table(self, table_name):
         """
-        Gets the table object with the given table name.
+        Get the table object with the given table name.
 
-        Often used so that a table object can be accessed, appended, and turned into a pandas DataFrame
+        Often used so that a table object can be accessed, appended, and turned into a pandas DataFrame.
 
         Args:
-            table_name (str): the name of the table that you want to access
+            table_name: the name of the table that you want to access
         Raises:
             ValueError: If the Table has not been made before calling get_table
         Returns:
-            the_returned_tab (Table): The new Table object pointing to table_name in the SQL file
+            the_returned_tab (Table): The Table object associated with the passed table_name
         """
         with self.conn:
-            table_there = False
-            c = self.conn.cursor()
-            sql_get_names = "SELECT table_name FROM meta_table"
-            for row in c.execute(sql_get_names):
-                if row[0] == table_name:
-                    table_there = True
-            if table_there:
-                sql_find_ltime_rtime = '''SELECT l_time_column, r_time_column, table_name FROM meta_table WHERE
-                table_name IN ("{tn}");'''.format(tn=table_name)
-                for row in c.execute(sql_find_ltime_rtime):
-                    l_time_ = str(row[0])
-                    r_time_ = str(row[1]) # keeping track of the logical and real time column names
+            curs = self.conn.cursor()
+            sql_get_table = f"SELECT table_name, l_time_column, r_time_column FROM meta_table WHERE table_name = ?;"
+            curs.execute(sql_get_table, (table_name,))
+            tup = curs.fetchone()
+
+            # Keep track of logical time and real time col names
+            if tup is not None:
+                l_time_ = str(tup[1])
+                r_time_ = str(tup[2]) 
             else:
                 raise ValueError("This table hasn't been made yet. Make this table before getting it")
         the_returned_tab = Table(self.conn, table_name, l_time_, r_time_)
@@ -143,16 +125,14 @@ class Database:
 
     def remove_table(self, table_name):
         """
-        Removes the table from the SQL file and deletes its information from the meta_table file.
+        Remove the table from the SQL file and delete its information from the meta_table file.
 
         Args:
-            table_name (str): the name of the table that you want to delete
-        Returns:
-            None
+            table_name: the name of the table that will be deleted
         """
         with self.conn:
-            c = self.conn.cursor()
+            curs = self.conn.cursor()
             sql_drop_table = "DROP TABLE IF EXISTS {tn};".format(tn=table_name)
             sql_delete_from_meta = 'DELETE FROM meta_table WHERE table_name="{tn}"'.format(tn=table_name)
-            c.execute(sql_drop_table)
-            c.execute(sql_delete_from_meta)
+            curs.execute(sql_drop_table)
+            curs.execute(sql_delete_from_meta)
--- simdash/database/table.py
+++ simdash/database/table.py
@@ -1,8 +1,8 @@
 """
-Tables are associated with specific tables in an SQL database.
+A Table is temporal dataframe with values associated with changing time.
 
-Tables allow users to append to these SQL tables in a variety of ways as well as
-turn the table into a pandas dataframe and access the table's length.
+A Table is a collection of values associated with time.
+It is the data structure used to store changing values.
 """
 
 import json
@@ -18,38 +18,36 @@ class Table:
     Attributes:
         table_name: the name of the table
         columns: list of data columns
-        l_column: the name of the logical time column
-        r_column: the name of the real time column
     """
-
     def __init__(self, conn, table_name, l_column, r_column):
         self.table_name = table_name
-        self.l_column = l_column
-        self.r_column = r_column
-
-        self.conn = conn
+        self.l_column_ = l_column
+        self.r_column_ = r_column
+        self.conn_ = conn
 
         # Load the column names
-        with self.conn:
-            sql = f"SELECT columns from meta_table where table_name = ?"
-            cur = self.conn.execute(sql, (self.table_name,))
+        with self.conn_:
+            sql = f"SELECT columns from meta_table where table_name = ?;"
+            cur = self.conn_.execute(sql, (self.table_name,))
             columns = cur.fetchone()[0]
             columns = json.loads(columns)
             self.columns = columns
 
         # Load the max logical time
-        with self.conn:
-            sql = f"SELECT max({self.l_column}) FROM {self.table_name}"
-            cur = self.conn.execute(sql)
+        with self.conn_:
+            sql = f"SELECT max({self.l_column_}) FROM {self.table_name};"
+            cur = self.conn_.execute(sql)
             try:
-                self.logical_time = cur.fetchone()[0]
+                self.logical_time = float(cur.fetchone()[0])
+                if self.logical_time is None:
+                    self.logical_time = 0.0
             except TypeError:
                 self.logical_time = 0.0
 
         # Construct the sql
-        sql = f"INSERT INTO {self.table_name} VALUES (%s)"
+        sql = f"INSERT INTO {self.table_name} VALUES (%s);"
         sql = sql % (",".join(["?"] * len(self.columns)))
-        self.insert_sql = sql
+        self.insert_sql_ = sql
 
     def append(self, l_time=None, r_time=None, *args, **kwargs):
         """
@@ -59,10 +57,6 @@ class Table:
         If the Logical time is not specified, the highest logical time in the table
         will be incremented by 1.  If real time is not specified, the real time
         will be taken as a pandas timestamp with the current local time.
-
-        logical_time (float): holds the value of the highest logical time in the table
-            so that when an append is called without specifying the l_time or logical time
-            column, this is incremented by 1 and attached to the data point
 
         Args:
             l_time (float): the logical time of the data point that is being uploaded,
@@ -75,61 +69,62 @@ class Table:
         """
 
         if l_time is not None:
-            if self.l_column in kwargs:
-                raise ValueError(f"Only one of l_time or {self.l_column} must be specified")
-            kwargs[self.l_column] = l_time
+            if self.l_column_ in kwargs:
+                raise ValueError(f"Only one of l_time or {self.l_column_} must be specified")
+            kwargs[self.l_column_] = l_time
 
         if r_time is not None:
-            if self.r_column in kwargs:
-                raise ValueError(f"Only one of r_time or {self.r_column} must be specified")
-            kwargs[self.r_column] = r_time
+            if self.r_column_ in kwargs:
+                raise ValueError(f"Only one of r_time or {self.r_column_} must be specified")
+            kwargs[self.r_column_] = r_time
 
-        for k, v in zip(self.columns[2:], args):
-            if k in kwargs:
-                raise ValueError(f"Value for columne {k} is specific twice")
-            kwargs[k] = v
+        for key, val in zip(self.columns[2:], args):
+            if key in kwargs:
+                raise ValueError(f"Value for column {key} is specified twice")
+            kwargs[key] = val
 
         # Convert rtime to timestamp
-        r_time = kwargs.get(self.r_column, None)
+        if r_time is None:
+            r_time = kwargs.get(self.r_column_, None)
         if r_time is None:
             r_time = pd.Timestamp("now")
         if not isinstance(r_time, pd.Timestamp):
             r_time = pd.Timestamp(r_time)
         r_time = r_time.timestamp()
-        kwargs[self.r_column] = r_time
+        kwargs[self.r_column_] = r_time
 
         # Set default l_time
-        if self.l_column not in kwargs:
-            kwargs[self.l_column] = self.logical_time + 1.0
-        self.logical_time = kwargs[self.l_column]
+        if self.l_column_ not in kwargs:
+            kwargs[self.l_column_] = self.logical_time + 1.0
+        self.logical_time = kwargs[self.l_column_]
 
         # Construct the parameters
-        params = (kwargs.get(param, None) for param in self.columns)
+        params = tuple(kwargs.get(param, None) for param in self.columns)
 
         # Execute the sql
-        with self.conn:
-            self.conn.execute(self.insert_sql, params)
+        with self.conn_:
+            self.conn_.execute(self.insert_sql_, params)
 
     def len(self):
         """
-        Determines length of the table in the form of length of the pandas dataframe (number of data points).
+        Get the length of the table which is equivalent to the number of datapoints
 
         Returns:
             the_length (int): the length of the table
         """
         query = 'SELECT * FROM "%s"' %self.table_name
-        dframe = pd.read_sql(query, self.conn)
+        dframe = pd.read_sql(query, self.conn_)
         the_length = len(dframe.index)
         return the_length
 
     def to_pandas(self):
         """
-        Creates and returns a  Pandas DataFrame (reindexed so that any of its values can be used in charts).
+        Creates and returns a Pandas DataFrame (reindexed so that any of its values can be used in charts).
 
         Returns:
             dframe (pd.DataFrame): a Pandas DataFrame with all of the data from the table of the SQL file
         """
         query = 'SELECT * FROM "%s"' %self.table_name
-        dframe = pd.read_sql(query, self.conn)
+        dframe = pd.read_sql(query, self.conn_)
         dframe.reindex()
         return dframe
